bda90d53dd71b6c0a3902de444c3e813c245abb4
chenjy <chenjy@g.ucla.edu>
2018-03-12 00:41:18 -0700

    initial outlines

---
+++ cmake-build-debug/CMakeFiles/plates_shells.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/chenjingyu/git/plates-shells/geometry.cpp" "/Users/chenjingyu/git/plates-shells/cmake-build-debug/CMakeFiles/plates_shells.dir/geometry.cpp.o"
+  "/Users/chenjingyu/git/plates-shells/main.cpp" "/Users/chenjingyu/git/plates-shells/cmake-build-debug/CMakeFiles/plates_shells.dir/main.cpp.o"
+  "/Users/chenjingyu/git/plates-shells/parameters.cpp" "/Users/chenjingyu/git/plates-shells/cmake-build-debug/CMakeFiles/plates_shells.dir/parameters.cpp.o"
+  "/Users/chenjingyu/git/plates-shells/pre_processor.cpp" "/Users/chenjingyu/git/plates-shells/cmake-build-debug/CMakeFiles/plates_shells.dir/pre_processor.cpp.o"
+  "/Users/chenjingyu/git/plates-shells/solver.cpp" "/Users/chenjingyu/git/plates-shells/cmake-build-debug/CMakeFiles/plates_shells.dir/solver.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
