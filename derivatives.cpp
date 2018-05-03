31f2d663026f421e67747a1db03a33d83b59be8d
chenjy <chenjy@g.ucla.edu>
2018-05-03 01:58:04 -0700

    organize

---
+++ derivatives.cpp
@@ -0,0 +1,5 @@
+//
+// Created by RLA on 2018/5/2.
+//
+
+#include "derivatives.h"
---
+++ derivatives.h
@@ -0,0 +1,8 @@
+//
+// Created by RLA on 2018/5/2.
+//
+
+#ifndef PLATES_SHELLS_DERIVATIVES_H
+#define PLATES_SHELLS_DERIVATIVES_H
+
+#endif //PLATES_SHELLS_DERIVATIVES_H
